80c46ad035e582c8f0c4ea9af443eb8316ac8ceb
小东门儿 <hbbdsqd@163.com>
2016-05-10 13:40:28 +0800

    123
    123

---
+++ 手势解锁demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  手势解锁demo
+//
+//  Created by 苏秋东 on 16/5/10.
+//  Copyright © 2016年 com.hyde.carelink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 手势解锁demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  手势解锁demo
+//
+//  Created by 苏秋东 on 16/5/10.
+//  Copyright © 2016年 com.hyde.carelink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
